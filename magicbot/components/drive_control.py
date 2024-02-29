febc26bfecbc112bb0c4d0d0c383f7a9b2239752
Combustible Lemons <lemons@frc5113.com>
2024-02-29 16:04:40 -0500

    idk brian was bothering me

---
+++ magicbot/components/drive_control.py
@@ -0,0 +1,62 @@
+import wpimath.controller
+import magicbot
+from magicbot.state_machine import state
+from magicbot import tunable
+import navx
+
+from components.Drivetrain import Drivetrain
+
+
+
+class DriveControl(magicbot.StateMachine):
+    # other components
+    drivetrain: Drivetrain
+
+    # variables to be injected
+    navx: navx.AHRS
+
+    """`tunable` numbers can be modified via Network Tables, and their value
+    is stored locally on the robot so it will be kept even after the robot
+    reboots. Also, this allows two different robots to have the same code
+    and different configuration values.
+    """
+    turn_to_angle_kP = tunable(0.03)
+    turn_to_angle_kI = tunable(0)
+    turn_to_angle_kD = tunable(0)
+
+    def setup(self):
+        self.turn_to_angle_controller = wpimath.controller.PIDController(
+            self.turn_to_angle_kP, self.turn_to_angle_kI, self.turn_to_angle_kD
+        )
+        self.turn_to_angle_controller.enableContinuousInput(0, 360)
+
+    def set_angle(self, angle: float):
+        self.turn_to_angle_controller.setSetpoint(angle)
+
+    def turn_to_angle(self) -> None:
+        """Robot turns to set angle using a PID controller.
+
+        This is a control function for the state machine, meaning that it,
+        and not the state itself, must be called to engage it.
+        """
+        self.engage(initial_state="turning_to_angle")
+
+    def turn_to_angle(self, angle: float) -> None:
+        """Robot turns to set angle using a PID controller.
+
+        This is a control function for the state machine, meaning that it,
+        and not the state itself, must be called to engage it.
+        """
+        self.set_angle(angle)
+        self.engage(initial_state="turning_to_angle")
+
+    @state(first=True)
+    def turning_to_angle(self):
+        # update controller parameters with new data from networktables
+        self.turn_to_angle_controller.setPID(
+            self.turn_to_angle_kP, self.turn_to_angle_kI, self.turn_to_angle_kD
+        )
+
+        measurement = self.navx.getAngle()
+        output = self.turn_to_angle_controller.calculate(measurement)
+        self.drivetrain.arcadeDrive(0,(output, -0.3, 0.3))
--- magicbot/robot.py
+++ magicbot/robot.py
@@ -2,20 +2,22 @@ import magicbot
 import wpilib
 import wpilib.drive
 import phoenix5
-from components.drivetrain import Drivetrain
+from components.Drivetrain import Drivetrain
 from wpilib import SmartDashboard
 import navx
-
+from components.drive_control import DriveControl
 
 class MyRobot(magicbot.MagicRobot):
     drivetrain: Drivetrain
+    drive_control: DriveControl
+    
 
     def createObjects(self):
         self.frontleftmotor = phoenix5.WPI_TalonSRX(15)
         self.frontrightmotor = phoenix5.WPI_TalonSRX(18)
         self.backleftmotor = phoenix5.WPI_TalonSRX(55)
         self.backrightmotor = phoenix5.WPI_TalonSRX(12)
-        self.joy = wpilib.Joystick(0)
+        self.joy = wpilib.XboxController(0)
         self.navx = navx.AHRS.create_spi()
 
     @property
@@ -23,9 +25,13 @@ class MyRobot(magicbot.MagicRobot):
         return self.navx.getAngle()
 
     def teleopPeriodic(self):
-        self.drivetrain.arcadeDrive(
-            -self.joy.getX() * 0.5, -self.joy.getY() * -0.5
-        )
+        with self.consumeExceptions():
+            self.drivetrain.arcadeDrive(
+                self.joy.getLeftY() * 0.6,
+                -self.joy.getLeftX() * 0.6
+            )
+        if self.joy.getAButton():
+            self.drive_control.turn_to_angle(180)
         SmartDashboard.putNumber("Joystick X value", self.joy.getY())
         SmartDashboard.putNumber("Joystick Y value", self.joy.getX())
         SmartDashboard.putNumber("navx", self.navx.getAngle())
