c338635a1e19f38b49adf695e27ba2f2e220c412
Ryanforce08 <rradtke1208@gmail.com>
2024-03-05 08:22:31 -0500

    formated and better comments

--- magicbot/components/Drivetrain.py
+++ magicbot/components/Drivetrain.py
@@ -6,31 +6,36 @@ from magicbot import feedback
 
 
 class Drivetrain:
-    # Sets what the motors controllers are
+    # Motor controllers for the drivetrain
     frontleftmotor: phoenix5.WPI_TalonSRX
     frontrightmotor: phoenix5.WPI_TalonSRX
     backleftmotor: phoenix5.WPI_TalonSRX
     backrightmotor: phoenix5.WPI_TalonSRX
 
+    # Variables to reset to during each control loop iteration
     forward = will_reset_to(0)
     turn = will_reset_to(0)
 
-    # sets up the motor groups
+    # Initialization setup function
     def setup(self):
+        # Create motor controller groups for the right and left side
         self.rightmotor = wpilib.MotorControllerGroup(
             self.frontrightmotor, self.backrightmotor
         )
         self.leftmotor = wpilib.MotorControllerGroup(
             self.frontleftmotor, self.backleftmotor
         )
+
+        # Create differential drive object for controlling the robot
         self.robotDrive = wpilib.drive.DifferentialDrive(
             self.leftmotor, self.rightmotor
         )
 
-    # sets drive type
+    # Method to set forward and turn values for arcade drive
     def arcadeDrive(self, forward, turn):
         self.forward = forward
         self.turn = turn
 
+    # Method to execute arcade drive during each control loop iteration
     def execute(self):
         self.robotDrive.arcadeDrive(self.forward, self.turn)
--- magicbot/robot.py
+++ magicbot/robot.py
@@ -9,33 +9,50 @@ from components.drive_control import DriveControl
 
 
 class MyRobot(magicbot.MagicRobot):
+    # Define robot components
     drivetrain: Drivetrain
     drive_control: DriveControl
-    #creates objects as the def implies
+
+    # Creates objects as the definition implies
     def createObjects(self):
+        # Motor controllers for the drivetrain
         self.frontleftmotor = phoenix5.WPI_TalonSRX(15)
         self.frontrightmotor = phoenix5.WPI_TalonSRX(18)
         self.backleftmotor = phoenix5.WPI_TalonSRX(55)
         self.backrightmotor = phoenix5.WPI_TalonSRX(12)
+
+        # Joystick for user input
         self.joy = wpilib.Joystick(0)
+
+        # Gyroscope sensor for orientation tracking
         self.navx = navx.AHRS.create_spi()
-        self.speed=self.joy.getThrottle()
+
+        # Throttle value from joystick for controlling speed
+        self.speed = self.joy.getThrottle()
 
     @property
     def get_angle(self):
         return self.navx.getAngle()
-    #everything under here does stuff when robot is running
+
+    # Teleop periodic function, called periodically during teleoperated mode
     def teleopPeriodic(self):
+        # Adjusting speed based on joystick input
         self.speed = float(self.joy.getThrottle())
         self.speed += float(1)
         self.speed / float(2)
-        self.speed +=float(0.5)
+        self.speed += float(0.5)
+
+        # Drive the robot using arcade drive with adjusted speed
         self.drivetrain.arcadeDrive(
-                -self.joy.getX() * float(self.speed),
-                self.joy.getY() * float(self.speed),
-            )
+            -self.joy.getX() * float(self.speed),
+            self.joy.getY() * float(self.speed),
+        )
+
+        # If trigger on joystick is pressed, command the robot to turn to a specific angle
         if self.joy.getTrigger():
             self.drive_control.turn_to_angle(180)
+
+        # Update SmartDashboard with joystick values and current gyroscope angle
         SmartDashboard.putNumber("Joystick X value", self.joy.getY())
         SmartDashboard.putNumber("Joystick Y value", self.joy.getX())
         SmartDashboard.putNumber("navx", self.navx.getAngle())
